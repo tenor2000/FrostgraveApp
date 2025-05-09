4745a63b59f53fd874a3f7b886a01e4d69c91f0f
tenor2000 <tenor2000@gmail.com>
2025-05-09 14:32:46 -0400

    basic tables complete

--- src/pages/reference/Creature.tsx
+++ src/pages/reference/Creature.tsx
@@ -1,5 +1,96 @@
-import React from "react";
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import { useReferenceData } from "../../context/ReferenceDataContext";
+
+type Creature = {
+  class: string;
+  type: number;
+  move: number;
+  fight: number;
+  shoot: number;
+  armor: number;
+  will: number;
+  health: number;
+  notes: string;
+  source: string;
+};
 
 export default function Creature() {
-  return <div>Creature</div>;
+  const { referenceData, loading, error } = useReferenceData();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error loading data</div>;
+  }
+
+  const objectArray: Creature[] = referenceData.creature_data;
+
+  return (
+    <Paper sx={{ width: "100%", overflow: "hidden" }}>
+      <TableContainer sx={{ maxHeight: 640 }}>
+        <Table
+          stickyHeader
+          aria-label="sticky table"
+          size="small"
+          sx={{
+            "& thead th": {
+              textAlign: "left",
+            },
+          }}
+        >
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Type</TableCell>
+              <TableCell>Move</TableCell>
+              <TableCell>Fight</TableCell>
+              <TableCell>Shoot</TableCell>
+              <TableCell>Armor</TableCell>
+              <TableCell>Will</TableCell>
+              <TableCell>Health</TableCell>
+              <TableCell>Notes</TableCell>
+              <TableCell>Source</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {objectArray.map((object: Creature) => (
+              <TableRow key={object.class}>
+                <TableCell>{object.class}</TableCell>
+                <TableCell>{object.type}</TableCell>
+                <TableCell sx={{ textAlign: "center" }}>
+                  {object.move}
+                </TableCell>
+                <TableCell sx={{ textAlign: "center" }}>
+                  {object.fight}
+                </TableCell>
+                <TableCell sx={{ textAlign: "center" }}>
+                  {object.shoot}
+                </TableCell>
+                <TableCell sx={{ textAlign: "center" }}>
+                  {object.armor}
+                </TableCell>
+                <TableCell sx={{ textAlign: "center" }}>
+                  {object.will}
+                </TableCell>
+                <TableCell sx={{ textAlign: "center" }}>
+                  {object.health}
+                </TableCell>
+                <TableCell>{object.notes}</TableCell>
+                <TableCell>{object.source}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Paper>
+  );
 }
--- src/pages/reference/Soldier.tsx
+++ src/pages/reference/Soldier.tsx
@@ -1,5 +1,106 @@
-import React from "react";
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import { useReferenceData } from "../../context/ReferenceDataContext";
+
+type Soldier = {
+  class: string;
+  type: string;
+  move: number;
+  fight: number;
+  shoot: number;
+  armor: number;
+  will: number;
+  health: number;
+  cost: number;
+  permItemSlots: number[];
+  notes: string | null;
+  source: string;
+};
 
 export default function Soldier() {
-  return <div>Soldier</div>;
+  const { referenceData, loading, error } = useReferenceData();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error loading data</div>;
+  }
+
+  const objectArray: Soldier[] = referenceData.soldier_data;
+
+  return (
+    <Paper sx={{ width: "100%", overflow: "hidden" }}>
+      <TableContainer sx={{ maxHeight: 640 }}>
+        <Table
+          stickyHeader
+          aria-label="sticky table"
+          size="small"
+          sx={{
+            "& thead th": {
+              textAlign: "left",
+            },
+          }}
+        >
+          <TableHead>
+            <TableRow>
+              <TableCell>Class</TableCell>
+              <TableCell>Move</TableCell>
+              <TableCell>Fight</TableCell>
+              <TableCell>Shoot</TableCell>
+              <TableCell>Armor</TableCell>
+              <TableCell>Will</TableCell>
+              <TableCell>Health</TableCell>
+              <TableCell>Cost</TableCell>
+              <TableCell>Items</TableCell>
+              <TableCell>Type</TableCell>
+              <TableCell>Notes</TableCell>
+              <TableCell>Source</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {objectArray.map((object: Soldier) => (
+              <TableRow key={object.class}>
+                <TableCell>{object.class}</TableCell>
+                <TableCell sx={{ textAlign: "center" }}>
+                  {object.move}
+                </TableCell>
+                <TableCell sx={{ textAlign: "center" }}>
+                  {object.fight}
+                </TableCell>
+                <TableCell sx={{ textAlign: "center" }}>
+                  {object.shoot}
+                </TableCell>
+                <TableCell sx={{ textAlign: "center" }}>
+                  {object.armor}
+                </TableCell>
+                <TableCell sx={{ textAlign: "center" }}>
+                  {object.will}
+                </TableCell>
+                <TableCell sx={{ textAlign: "center" }}>
+                  {object.health}
+                </TableCell>
+                <TableCell sx={{ textAlign: "center" }}>
+                  {object.cost} gc
+                </TableCell>
+                <TableCell sx={{ textAlign: "center" }}>
+                  {object.permItemSlots}
+                </TableCell>
+                <TableCell>{object.type}</TableCell>
+                <TableCell>{object.notes}</TableCell>
+                <TableCell>{object.source}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Paper>
+  );
 }
