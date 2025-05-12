99bae7ac0381724425292fcb48ffc1496846bc19
tenor2000 <tenor2000@gmail.com>
2025-05-12 11:15:14 -0400

    new reference table

--- src/pages/reference/Weapon.tsx
+++ src/pages/reference/Weapon.tsx
@@ -1,5 +1,74 @@
-import React from "react";
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import { useReferenceData } from "../../context/ReferenceDataContext";
+import modSign from "../../utilFunctions/modSign";
+
+type Weapon = {
+  name: string;
+  damageMod: number;
+  maxRange: number;
+  notes: string;
+  source: string;
+};
 
 export default function Weapon() {
-  return <div>Weapon</div>;
+  const { referenceData, loading, error } = useReferenceData();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error loading data</div>;
+  }
+
+  const objectArray: Weapon[] = referenceData.weapon_data;
+
+  return (
+    <Paper sx={{ width: "100%", overflow: "hidden" }}>
+      <TableContainer sx={{ maxHeight: 640 }}>
+        <Table
+          stickyHeader
+          aria-label="sticky table"
+          size="small"
+          sx={{
+            "& thead th": {
+              textAlign: "left",
+            },
+          }}
+        >
+          <TableHead>
+            <TableRow>
+              <TableCell>Type</TableCell>
+              <TableCell>Damage Mod</TableCell>
+              <TableCell>Max Range</TableCell>
+              <TableCell>Notes</TableCell>
+              <TableCell>Source</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {objectArray.map((object: Weapon) => (
+              <TableRow key={object.name}>
+                <TableCell>{object.name}</TableCell>
+                <TableCell sx={{ textAlign: "center" }}>
+                  {modSign(object.damageMod)}
+                </TableCell>
+                <TableCell sx={{ textAlign: "center" }}>
+                  {object.maxRange}
+                </TableCell>
+                <TableCell>{object.notes}</TableCell>
+                <TableCell>{object.source}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Paper>
+  );
 }
