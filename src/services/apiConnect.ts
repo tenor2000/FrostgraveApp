a5a65b72f34f6decb3b5c265ed859ebfe04a5748
tenor2000 <tenor2000@gmail.com>
2025-05-13 13:20:09 -0400

    added more connection functions

--- src/services/apiConnect.ts
+++ src/services/apiConnect.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-// const backendUrl = import.meta.env.VITE_BACKEND_URL;
+import type { User } from "../context/AuthContext";
 const backendUrl = "http://localhost:5050/api";
 
 export const getReferenceData = async () => {
@@ -8,8 +8,27 @@ export const getReferenceData = async () => {
   return response;
 };
 
-export const getUserData = async () => {
-  const response = await axios.get(`${backendUrl}/user/data`);
+// export const getWarbandData = async () => {
+//   const response = await axios.get(`${backendUrl}/warbands/data`);
+//   console.log(response);
+//   return response;
+// };
+
+export const getUserData = async (token: string) => {
+  const response: User = await axios.get(`${backendUrl}/users`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  console.log(response);
+  return response;
+};
+
+export const loginAPI = async (username: string, password: string) => {
+  const response = await axios.post(`${backendUrl}/auth/login`, {
+    username,
+    password,
+  });
   console.log(response);
   return response;
 };
