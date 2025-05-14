83f5343896ecba4710b5f531a577c328959b79b5
tenor2000 <tenor2000@gmail.com>
2025-05-14 15:31:46 -0400

    create post to create wizard in either localstorage or through the api

--- src/services/apiConnect.ts
+++ src/services/apiConnect.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 const backendUrl = import.meta.env.VITE_BACKEND_URL;
+import type { Wizard } from "../pages/warbands/NewWizardForm";
 
 export const getReferenceData = async () => {
   const response = await axios.get(`${backendUrl}/reference/data`);
@@ -12,3 +13,26 @@ export const getReferenceData = async () => {
 //   console.log(response);
 //   return response;
 // };
+
+export const postNewWizard = async (wizard: Wizard) => {
+  if (wizard.user_id !== "LocalStorage") {
+    try {
+      const response = await axios.post(
+        `${backendUrl}/warbands/wizards`,
+        wizard
+      );
+      console.log(response.data);
+      return response.data;
+    } catch (err: any) {
+      console.error(err);
+    }
+  } else {
+    try {
+      localStorage.setItem("wizardFG", JSON.stringify(wizard));
+      console.log("Saved to localStorage");
+      return wizard;
+    } catch (err: any) {
+      console.error(err);
+    }
+  }
+};
