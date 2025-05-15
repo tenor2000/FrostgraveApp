a3d63f384c35b39fa793c1cac2f5b1c7ad23cc7e
tenor2000 <tenor2000@gmail.com>
2025-05-14 23:48:55 -0400

    Some temp stuff

--- src/pages/warbands/Warbands.tsx
+++ src/pages/warbands/Warbands.tsx
@@ -1,5 +1,36 @@
-import React from "react";
+import { Box, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import { useAuthData } from "../../context/AuthContext";
 
 export default function Warbands() {
-  return <div>Warbands</div>;
+  const { user, loading, error } = useAuthData();
+  const { warbandData } = useAuthData();
+
+  if (loading) return <p>Loading...</p>;
+  // if (error) return <p>{error.message}</p>;
+  console.log(warbandData);
+
+  if (!user) {
+    return (
+      <Box>
+        <Typography>Warbands</Typography>
+        <Link to="/users/login">Login</Link>
+      </Box>
+    );
+  }
+
+  return (
+    <Box>
+      <Typography>Warbands</Typography>
+      {user && <p>{user.username}</p>}
+      {warbandData &&
+        warbandData
+          .sort((a, b) =>
+            a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1
+          )
+          .map((wizard) => <p key={wizard._id}>{wizard.name}</p>)}
+
+      <Link to="/warbands/createWizard">New Wizard</Link>
+    </Box>
+  );
 }
