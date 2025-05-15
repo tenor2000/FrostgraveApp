3402c0d082edd6e434d2ba744e891606adf4b3d0
tenor2000 <tenor2000@gmail.com>
2025-05-14 22:39:48 -0400

    forgot to return axios call

--- src/services/deleteFollower.ts
+++ src/services/deleteFollower.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default function deleteFollower(token: string, follower_id: string) {
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
-  axios.delete(`${backendUrl}/warbands/followers/${follower_id}`, {
+  return axios.delete(`${backendUrl}/warbands/followers/${follower_id}`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
--- src/services/deleteWizard.ts
+++ src/services/deleteWizard.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default function deleteWizard(token: string, wizard_id: string) {
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
-  axios.delete(`${backendUrl}/warbands/wizards/${wizard_id}`, {
+  return axios.delete(`${backendUrl}/warbands/wizards/${wizard_id}`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
