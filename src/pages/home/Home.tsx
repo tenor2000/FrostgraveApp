61f8954a50162f1362c461733d67bc2d6717518c
tenor2000 <tenor2000@gmail.com>
2025-05-15 17:28:53 -0400

    added a picture

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,16 @@
 import { useAuthData } from "../../context/AuthContext";
+import { Box, Typography } from "@mui/material";
 
 export default function Home() {
   const { user } = useAuthData();
   return (
-    <>
-      <h1>Home</h1>
-      {user && <p>{user.username}</p>}
-    </>
+    <Box sx={{ padding: "1rem" }}>
+      <Typography variant="h3">Welcome, {user?.username || "Guest"}</Typography>
+      <img
+        src="https://res.cloudinary.com/bloomsbury-atlas/image/upload/w_568,c_scale,dpr_1.5/jackets/9781472834683.jpg"
+        alt="frostgrave 2nd edition book cover"
+        style={{ height: "60vh", width: "auto", margin: "25px" }}
+      />
+    </Box>
   );
 }
