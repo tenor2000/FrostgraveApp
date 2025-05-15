c502c3dccc62feec2924b3bb4878ccbfd89e109d
tenor2000 <tenor2000@gmail.com>
2025-05-15 11:43:01 -0400

    made more independent

--- src/utilFunctions/getItemFromId.ts
+++ src/utilFunctions/getItemFromId.ts
@@ -1,5 +1,8 @@
-export function getItemFromId(id: number, refData: object) {
-  return Object.values(refData)
+import { useReferenceData } from "../context/ReferenceDataContext";
+
+export function getItemFromId(id: number) {
+  const { referenceData } = useReferenceData();
+  return Object.values(referenceData)
     .flat()
     .filter((obj) => obj.item_id)
     .find((item) => item.item_id == id);
