516f95c1b83905a8115c7b451db99fee1fc3b0c6
tenor2000 <tenor2000@gmail.com>
2025-05-14 22:46:23 -0400

    corrected endpoint

--- src/services/deleteApprentice.ts
+++ src/services/deleteApprentice.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-export default function deleteWizard(token: string, wizard_id: string) {
+export default function deleteWizard(token: string, apprentice_id: string) {
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
-  return axios.delete(`${backendUrl}/warbands/wizards/${wizard_id}`, {
+  return axios.delete(`${backendUrl}/warbands/apprentices/${apprentice_id}`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
