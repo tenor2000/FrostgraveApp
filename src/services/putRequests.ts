2c2a30f8e2d301a978674fe209bc5960ea97ae6a
tenor2000 <tenor2000@gmail.com>
2025-05-15 21:18:29 -0400

    removed token parameter requirements

--- src/services/putRequests.ts
+++ src/services/putRequests.ts
@@ -1,49 +1,50 @@
 import axios from "axios";
 import type { User } from "../types/UserTypes";
 import type { Wizard, Apprentice, Follower } from "../types/WarbandTypes";
+import { getToken } from "./authToken";
 const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
-export const putUser = async (token: string, userData: User) => {
+export const putUser = async (userData: User) => {
   return axios.put(`${backendUrl}/users/${userData._id}`, userData, {
     headers: {
-      Authorization: `Bearer ${token}`,
+      Authorization: `Bearer ${getToken()}`,
     },
   });
 };
 
-export const putWizard = async (token: string, wizardData: Wizard) => {
+export const putWizard = async (wizardData: Wizard) => {
   return axios.put(
     `${backendUrl}/warbands/wizards/${wizardData._id}`,
     wizardData,
     {
       headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${getToken()}`,
       },
     }
   );
 };
 
-const putApprentice = async (token: string, apprenticeData: Apprentice) => {
+export const putApprentice = async (apprenticeData: Apprentice) => {
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
   return axios.put(
     `${backendUrl}/warbands/apprentices/${apprenticeData._id}`,
     apprenticeData,
     {
       headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${getToken()}`,
       },
     }
   );
 };
 
-export const putFollower = async (token: string, followerData: Follower) => {
+export const putFollower = async (followerData: Follower) => {
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
   return axios.put(
     `${backendUrl}/warbands/followers/${followerData._id}`,
     followerData,
     {
       headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${getToken()}`,
       },
     }
   );
